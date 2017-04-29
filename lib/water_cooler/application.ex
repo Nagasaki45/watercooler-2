e8ff71fda73e6a9dc148d18dc7f5dbcd8e987284
Tom Gurion <nagasaki45@gmail.com>
2017-04-29 19:31:38 +0100

    Add SSL endpoint

--- lib/water_cooler/application.ex
+++ lib/water_cooler/application.ex
@@ -5,7 +5,15 @@ defmodule WaterCooler.Application do
     import Supervisor.Spec, warn: false
 
     children = [
-      Plug.Adapters.Cowboy.child_spec(:http, WaterCooler, [], port: 8080)
+      Plug.Adapters.Cowboy.child_spec(:http, WaterCooler, [],
+        port: 8080,
+      ),
+      Plug.Adapters.Cowboy.child_spec(:https, WaterCooler, [],
+        port: 8443,
+        otp_app: :water_cooler,
+        keyfile: "priv/localhost.key",
+        certfile: "priv/localhost.cert",
+      ),
     ]
 
     opts = [strategy: :one_for_one, name: WaterCooler.Supervisor]
